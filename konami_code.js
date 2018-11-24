f27d0651e8440f8902bcfb6f8a74d0218cd42d7c
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-24 21:53:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,19 +12,21 @@ const codes = [
   "a"
 ];
 
-let index = 0;
-
-function init(){
-  document.addEventListener("keydown", function(event) {
-    console.log(event.key === codes[index]);
-    if(event.key === codes[index]){
-      index++;
-      if (index === codes.length) {
-        window.alert("Hooray!");
-        index = 0;
-      }
-    } else {
-      index = 0;
+let index = 0 // iinitialize 'index' variable, it will count the correct presses on the keyboard
+ function onKeyDownHandler(e) {
+  const key = e.key // assign event's key to 'key' variable
+  console.log("key: ", key)
+  if (key === codes[index]) { // check if the name of the key matches an array element
+    index++ // increment the key if it does
+     if (index === codes.length) { // check if index is to array's length. This means we pressed all the right keys
+      alert("Tada!") // alert the browser, the program works properly
+       index = 0 // reset the index
     }
-  });
+  } else {
+    index = 0 // wrong key pressed, reset the index to start over
+  }
+}
+ function init() { // initialize the program
+  // your code here
+  document.body.addEventListener("keydown", onKeyDownHandler) // attach 'keydown' event listener to document.body and pass the callback function
 }
